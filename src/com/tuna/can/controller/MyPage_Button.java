0541c8c13b11602482b860d7e216595202988a2f
kim-sunwoong <sw649666@gmail.com>
2021-04-05 18:35:38 +0900

    수정~sw

---
+++ src/com/tuna/can/controller/MyPage_Button.java
@@ -0,0 +1,35 @@
+package com.tuna.can.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class MyPage_Button extends JButton implements ActionListener{
+
+	private int price;
+	private int itemCode;
+	private JButton button = null;
+
+	public MyPage_Button() {
+
+	}
+	
+	public MyPage_Button(JButton button) {
+		this.button = button;
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		
+		
+		
+	}
+	
+	
+}
--- src/com/tuna/can/view/MyPage_sunwoong.java
+++ src/com/tuna/can/view/MyPage_sunwoong.java
@@ -11,18 +11,23 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.naming.NameAlreadyBoundException;
 import javax.swing.Icon;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import com.tuna.can.controller.MyPage_Button;
 
 public class MyPage_sunwoong {
 
@@ -78,9 +83,9 @@ public class MyPage_sunwoong {
       
       Label infoLabel = new Label("information");
       infoLabel.setBounds(20, 20, 200, 50);
-      infoLabel.setFont(new Font("test", Font.BOLD, 30));
+      infoLabel.setFont(new Font("SansSerif", Font.BOLD, 30));
       
-      Font font = new Font("woongFont",Font.BOLD, 18);
+      Font font = new Font("Centaur",Font.BOLD, 18);
       
       Label nameLabel = new Label("username");
       nameLabel.setBounds(20, 100, 90, 30);
@@ -132,7 +137,7 @@ public class MyPage_sunwoong {
       bottomPanel2.setLayout(null);
       bottomPanel2.setLocation(350, 250);
       bottomPanel2.setSize(350, 650);
-      bottomPanel2.setBackground(Color.gray);
+      bottomPanel2.setBackground(Color.pink);
       
 //      우측 하단 상단 패널
 //      버튼 클릭시 
@@ -177,122 +182,132 @@ public class MyPage_sunwoong {
       bottomTopPanel.add(myBackgroundButton);
       bottomTopPanel.add(myFontButton);
       
-      
 //      우측 하단 하단 패널
 //      new GridLayout(3, 2, 20, 20)
       CardLayout card = new CardLayout();
       
       bottomBottomPanel.setLayout(card);
       bottomBottomPanel.setLocation(0, 100);
-      bottomBottomPanel.setSize(350, 550);
+      bottomBottomPanel.setSize(350, 525);
       bottomBottomPanel.setBackground(Color.pink);
       
       JPanel characterPanel = new JPanel();
-      characterPanel.setLayout(null);
+      characterPanel.setLayout(new GridLayout(3,3));
       characterPanel.setLocation(0, 100);
-      characterPanel.setSize(350, 550);
-      characterPanel.setBackground(Color.pink);
-      
+      characterPanel.setSize(350, 525);
+      characterPanel.setBackground(Color.blue);
       
       JPanel backgroundPanel = new JPanel();
-      backgroundPanel.setLayout(null);
+      backgroundPanel.setLayout(new GridLayout(3,3));
       backgroundPanel.setLocation(0, 100);
-      backgroundPanel.setSize(350, 550);
+      backgroundPanel.setSize(350, 525);
       backgroundPanel.setBackground(Color.gray);
       
       JPanel fontPanel = new JPanel();
-      fontPanel.setLayout(null);
+      fontPanel.setLayout(new GridLayout(3,3));
       fontPanel.setLocation(0, 100);
-      fontPanel.setSize(350, 550);
+      fontPanel.setSize(350, 525);
       fontPanel.setBackground(Color.green);
       
+      for(int i = 1; i < 19; i++) {
+    	  if(i < 7) {
+    		  characterPanel.add(new MyPage_Button());
+    	  } else if(i < 13) {
+    		  backgroundPanel.add(new MyPage_Button());
+    	  } else {
+    		  fontPanel.add(new MyPage_Button());
+    	  }
+      }
       
-      JButton inventory1 = new JButton("1~6 ㅎㅎ");
-      inventory1.setBounds(35, 30, 120, 120);
-      JButton inventory2 = new JButton("위 클릭하면");
-      inventory2.setBounds(185, 30, 120, 120);
-      JButton inventory3 = new JButton("바뀜");
-      inventory3.setBounds(35, 180, 120, 120);
-      JButton inventory4 = new JButton();
-      inventory4.setBounds(185, 180, 120, 120);
-      JButton inventory5 = new JButton();
-      inventory5.setBounds(35, 330, 120, 120);
-      JButton inventory6 = new JButton();
-      inventory6.setBounds(185, 330, 120, 120);
-
-      JButton inventory7 = new JButton("7~12 ㅎㅎ");
-      inventory7.setBounds(35, 30, 120, 120);
-      JButton inventory8 = new JButton();
-      inventory8.setBounds(185, 30, 120, 120);
-      JButton inventory9 = new JButton();
-      inventory9.setBounds(35, 180, 120, 120);
-      JButton inventory10 = new JButton();
-      inventory10.setBounds(185, 180, 120, 120);
-      JButton inventory11 = new JButton();
-      inventory11.setBounds(35, 330, 120, 120);
-      JButton inventory12 = new JButton();
-      inventory12.setBounds(185, 330, 120, 120);
       
-      JButton inventory13 = new JButton("13~18 ㅎㅎ");
-      inventory13.setBounds(35, 30, 120, 120);
-      JButton inventory14 = new JButton();
-      inventory14.setBounds(185, 30, 120, 120);
-      JButton inventory15 = new JButton();
-      inventory15.setBounds(35, 180, 120, 120);
-      JButton inventory16 = new JButton();
-      inventory16.setBounds(185, 180, 120, 120);
-      JButton inventory17 = new JButton();
-      inventory17.setBounds(35, 330, 120, 120);
-      JButton inventory18 = new JButton();
-      inventory18.setBounds(185, 330, 120, 120);
+//      JButton inventory1 = new JButton("1~6 ㅎㅎ");
+//      inventory1.setBounds(35, 30, 120, 120);
+//      JButton inventory2 = new JButton("위 클릭하면");
+//      inventory2.setBounds(185, 30, 120, 120);
+//      JButton inventory3 = new JButton("바뀜");
+//      inventory3.setBounds(35, 180, 120, 120);
+//      JButton inventory4 = new JButton();
+//      inventory4.setBounds(185, 180, 120, 120);
+//      JButton inventory5 = new JButton();
+//      inventory5.setBounds(35, 330, 120, 120);
+//      JButton inventory6 = new JButton();
+//      inventory6.setBounds(185, 330, 120, 120);
+//
+//      JButton inventory7 = new JButton("7~12 ㅎㅎ");
+//      inventory7.setBounds(35, 30, 120, 120);
+//      JButton inventory8 = new JButton();
+//      inventory8.setBounds(185, 30, 120, 120);
+//      JButton inventory9 = new JButton();
+//      inventory9.setBounds(35, 180, 120, 120);
+//      JButton inventory10 = new JButton();
+//      inventory10.setBounds(185, 180, 120, 120);
+//      JButton inventory11 = new JButton();
+//      inventory11.setBounds(35, 330, 120, 120);
+//      JButton inventory12 = new JButton();
+//      inventory12.setBounds(185, 330, 120, 120);
+//      
+//      JButton inventory13 = new JButton("13~18 ㅎㅎ");
+//      inventory13.setBounds(35, 30, 120, 120);
+//      JButton inventory14 = new JButton();
+//      inventory14.setBounds(185, 30, 120, 120);
+//      JButton inventory15 = new JButton();
+//      inventory15.setBounds(35, 180, 120, 120);
+//      JButton inventory16 = new JButton();
+//      inventory16.setBounds(185, 180, 120, 120);
+//      JButton inventory17 = new JButton();
+//      inventory17.setBounds(35, 330, 120, 120);
+//      JButton inventory18 = new JButton();
+//      inventory18.setBounds(185, 330, 120, 120);
       
 //    인벤 1번 클릭시 이벤트
-      inventory1.addMouseListener(new MouseAdapter() {
-
-		@Override
-		public void mouseClicked(MouseEvent e) {
-
-		}
-    	  
-    	  
-	});
+//      inventory1.addMouseListener(new MouseAdapter() {
+//
+//		@Override
+//		public void mouseClicked(MouseEvent e) {
+//
+//		}
+//    	  
+//    	  
+//	});
       
       bottomBottomPanel.add("character", characterPanel);
       bottomBottomPanel.add("background", backgroundPanel);
       bottomBottomPanel.add("font", fontPanel);
       
-      characterPanel.add(inventory1);
-      characterPanel.add(inventory2);
-      characterPanel.add(inventory3);
-      characterPanel.add(inventory4);
-      characterPanel.add(inventory5);
-      characterPanel.add(inventory6);
-      
-      backgroundPanel.add(inventory7);
-      backgroundPanel.add(inventory8);
-      backgroundPanel.add(inventory9);
-      backgroundPanel.add(inventory10);
-      backgroundPanel.add(inventory11);
-      backgroundPanel.add(inventory12);
-      
-      fontPanel.add(inventory13);
-      fontPanel.add(inventory14);
-      fontPanel.add(inventory15);
-      fontPanel.add(inventory16);
-      fontPanel.add(inventory17);
-      fontPanel.add(inventory18);
+//      characterPanel.add(inventory1);
+//      characterPanel.add(inventory2);
+//      characterPanel.add(inventory3);
+//      characterPanel.add(inventory4);
+//      characterPanel.add(inventory5);
+//      characterPanel.add(inventory6);
+//      
+//      backgroundPanel.add(inventory7);
+//      backgroundPanel.add(inventory8);
+//      backgroundPanel.add(inventory9);
+//      backgroundPanel.add(inventory10);
+//      backgroundPanel.add(inventory11);
+//      backgroundPanel.add(inventory12);
+//      
+//      fontPanel.add(inventory13);
+//      fontPanel.add(inventory14);
+//      fontPanel.add(inventory15);
+//      fontPanel.add(inventory16);
+//      fontPanel.add(inventory17);
+//      fontPanel.add(inventory18);
       
       card.show(bottomBottomPanel, "character");
       
       
-      myCharacterButton.addMouseListener(new MouseAdapter() {
-
+      myCharacterButton.addActionListener(new ActionListener() {
+		
 		@Override
-		public void mouseClicked(MouseEvent e) {
+		public void actionPerformed(ActionEvent e) {
 
-			card.show(bottomBottomPanel, "character");
+			if(e.getSource() == myCharacterButton) {
+				card.show(bottomBottomPanel, "character");
+			}
+			
 		}
-    	  
 	});
       myBackgroundButton.addMouseListener(new MouseAdapter() {
 
