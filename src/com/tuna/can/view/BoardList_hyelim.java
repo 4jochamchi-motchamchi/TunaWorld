a8898edfa43616aff9832d337479ce5eb608c550
tj <tj@DESKTOP-MOB8F55>
2021-04-05 18:49:38 +0900

    이미지

--- src/com/tuna/can/view/BoardList_hyelim.java
+++ src/com/tuna/can/view/BoardList_hyelim.java
@@ -1,21 +1,43 @@
 package com.tuna.can.view;
 import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.Border;
 
+/**
+ * <pre>
+ * 게시판 목록 페이지
+ * </pre>
+ * @author Hyelim Jeon
+ *
+ */
 public class BoardList_hyelim {
 
 	public static void main(String[] args) {
 
+		//메인프레임생성
 		JFrame frame = new JFrame("BoardList");
 		frame.setLayout(null);
 		frame.setSize(700, 900);
 		
+		//아이콘
+		try {
+			frame.setIconImage(ImageIO.read(new File("image/logoBig.PNG")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//각위치 패널 생성
 		JPanel topPanel = new JPanel();
 		topPanel.setLayout(null);
 		topPanel.setBounds(0, 0, 700, 125);
@@ -34,8 +56,12 @@ public class BoardList_hyelim {
 	   
 		
 		// 뒤로가기 버튼
-		JButton backButton = new JButton(" back ");
-		backButton.setBounds(30, 30, 90, 30);
+	    ImageIcon home = new ImageIcon("image/home.PNG");
+		JButton backButton = new JButton(home);
+		Border pinkborder = BorderFactory.createLineBorder(Color.pink, 1);
+		backButton.setBounds(30, 30, 55, 55);
+		backButton.setBackground(Color.pink);
+		backButton.setBorder(pinkborder);
 		topPanel.add(backButton);
 		
 		//전체게시글 글씨
@@ -47,19 +73,18 @@ public class BoardList_hyelim {
 		//전체글 리스트 
         JList list = new JList();
         midlePanel.add(list);
+
         
-        
-		
-		
 		//스크롤
 		
-		JScrollPane scroll = new JScrollPane(list,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-			    JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        midlePanel.add(scroll);
+//		JScrollPane scroll = new JScrollPane(list,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+//			    JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+//		
+//        midlePanel.add(scroll);
+        JScrollPane a = new JScrollPane();
+        midlePanel.add(a);
 		
-		
-		
-		
+
 		//삭제버튼(확인팝업)
 		JButton deleteButton  = new JButton("delete");
 		deleteButton.addActionListener(null);
@@ -69,14 +94,9 @@ public class BoardList_hyelim {
 		//수정버튼
 	   
 	    
+
 		
-		
-	
-		//게시글목록
-		
-		
-		
-	    //내게시글보기 	버튼
+	    //내게시글보기 버튼
 		JButton myboard  = new JButton("myboard");
 		myboard.setBounds(30, 20, 90, 30);
 		bottonPanel.add(myboard);
@@ -97,5 +117,6 @@ public class BoardList_hyelim {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
+	
 
 }
--- src/com/tuna/can/view/TextArea_hyelim.java
+++ src/com/tuna/can/view/TextArea_hyelim.java
@@ -8,25 +8,44 @@ import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.border.Border;
 
+/**
+ * <pre>
+ * 게시판 글쓰기 페이지
+ * </pre>
+ * @author Hyelim Jeon
+ *
+ */
 public class TextArea_hyelim {
 	
 	public static void main(String[] args) {
 	
-        //메인프레임 생
+        //메인프레임 생성
 		JFrame frame = new JFrame("TextArea");
 		frame.setLayout(null);
 		frame.setSize(700, 900);
+		//아이콘
+		try {
+			frame.setIconImage(ImageIO.read(new File("image/logoBig.PNG")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		//각위치의 패널생
+		//각위치의 패널 생성
 		JPanel topPanel = new JPanel();
 		topPanel.setLayout(null);
 		topPanel.setBounds(0, 0, 700, 125);
@@ -58,8 +77,12 @@ public class TextArea_hyelim {
 		topPanel.add(lbl);
 
 		// 뒤로가기 버튼
-		JButton backButton = new JButton(" back ");
-		backButton.setBounds(30, 30, 90, 25);
+		ImageIcon home = new ImageIcon("image/home.PNG");
+		Border pinkborder = BorderFactory.createLineBorder(Color.pink, 1);
+		JButton backButton = new JButton(home);
+		backButton.setBackground(Color.pink);
+		backButton.setBorder(pinkborder);
+		backButton.setBounds(30, 30, 55, 55);
 		topPanel.add(backButton);
 		
 		
@@ -67,7 +90,7 @@ public class TextArea_hyelim {
 		JLabel subname = new JLabel("제목");
         subname.setBounds(50,10,90,25);
         TextField subject = new TextField(80);
-        subject.setBounds(150,10, 500 ,25);
+        subject.setBounds(140,10, 500 ,25);
         
         subP.add(subname);
         subP.add(subject);
@@ -75,23 +98,17 @@ public class TextArea_hyelim {
         
         //게시글
         TextArea txt = new TextArea(30,70);
-        txt.setBounds(50,0,600,500);
+        txt.setBounds(40,0,600,550);
         textareaP.add(txt);
         
         
 		//날짜
-       
-      
         Date day = new Date();
-        String today = day.toGMTString();
+        String today = day.toLocaleString();
         JLabel oneul = new JLabel(today);
-        oneul.setBounds(490, 0, 200, 30);
+        oneul.setBounds(500, 10, 200, 30);
         bottonP.add(oneul);
-        
-        
-     
-        
-        
+
         
         //공개범위 라디오 버튼
         JRadioButton myself = new JRadioButton("나만");
@@ -102,21 +119,22 @@ public class TextArea_hyelim {
 	    range.add(myself);
 	    range.add(friend);
 	    range.add(all);
-	    
-	    myself.setBounds(40, 80, 80,50);
-	    friend.setBounds(100, 80, 80, 50);
-	    all.setBounds(170, 80, 80, 50);
-	    
+	   
+	    myself.setBounds(50, 0, 60,50);
+	    friend.setBounds(105, 0, 70, 50);
+	    all.setBounds(170, 0, 80, 50);
+
 	    bottonP.add(myself);
 	    bottonP.add(friend);
 	    bottonP.add(all);
-	    
-        
-	    
+
+	    myself.setBackground(Color.pink);
+	    friend.setBackground(Color.pink);
+	    all.setBackground(Color.pink);
 
         //저장버튼
         JButton saveButton = new JButton(" save ");
-		saveButton.setBounds(580, 80 , 80, 25);
+		saveButton.setBounds(550, 80 , 80, 25);
 		bottonP.add(saveButton);
 		
 
