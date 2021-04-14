3d69f49f0a1fc12ea92c934c1a13c6421a0f3b14
juhee <fhqj21@gmail.com>
2021-04-14 23:33:38 +0900

    황주희23차

--- src/com/tuna/can/run/Application.java
+++ src/com/tuna/can/run/Application.java
@@ -9,21 +9,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		UserDTO ud = new UserDTO();
-		ud.setUserNo(1);
-		ud.setCoin(20);
 		
-		TunaController tc = new TunaController();
-//		tc.selectUSerCoin(ud);
-		tc.updateCoin(ud);
-		
-//		tc.selectPlusFriend(ud);
-		
-		AddFriendDTO ad = new AddFriendDTO();
-		ad.setUserNo(1);
-		ad.setRequsetFriendNo(4);
-		tc.RequestFriends(ad);
-		
-//		tc.RequestFriends(ad);
 	}
 }
