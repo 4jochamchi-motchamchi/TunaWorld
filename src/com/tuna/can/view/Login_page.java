69e49ce785e7e8f73c903c44a5dc78ce81ba5f7f
NaraWee <wnl1508@naver.com>
2021-04-14 18:35:54 +0900

    enter 누르면 로그인 됩니다 위나라

--- src/com/tuna/can/view/Login_page.java
+++ src/com/tuna/can/view/Login_page.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -137,18 +138,8 @@ public class Login_page extends JFrame{
 		loginPanel.add(loginBtn);
 		loginPanel.add(createUserBtn);
 
-		pwText.addKeyListener(new KeyListener() {
-
-			@Override
-			public void keyTyped(KeyEvent e) {
-
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-
-			}
-
+		pwText.addKeyListener(new KeyAdapter() {
+			
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
@@ -197,6 +188,9 @@ public class Login_page extends JFrame{
 				}
 			}
 		});
+		
+		
+		
 
 		loginBtn.addActionListener(new ActionListener() {
 
